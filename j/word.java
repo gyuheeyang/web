05542ee34a3276dea4f220c18227f9be7ae7d159
gyuheeyang <gyuheeyang@gmail.com>
2022-06-09 11:11:26 +0900

    java 모음 isVowel

---
+++ j/word.java
@@ -0,0 +1,21 @@
+
+public class Word {
+	public static void main(String[] arg) {
+		Word word = new Word("Hello World");
+		System.out.println(word.isVowel(3));
+	}
+	
+	
+	private String letters;
+	
+	public Word(String letters) {
+		this.letters = letters.toLowerCase();
+	}
+	
+	//i번째 글자가 모음인지 아닌지?? i번째 글자가 모음이라면 true ? 자음이라면 false??  
+	//a, i ,u, e, o
+	public boolean isVowel(int i) {
+		return letters.substring(i, i + 1).equals("a") || letters.substring(i, i + 1).equals("i") || letters.substring(i, i + 1).equals("u") || letters.substring(i, i + 1).equals("e") || letters.substring(i, i + 1).equals("o");	
+	}
+
+}
